d1297cf106c5fac143ca6f115a3c433dc23b578c
farjamazizi <46857447+farjamazizi@users.noreply.github.com>
2019-01-21 23:29:10 +0330

    step1.1(intro) project.py

--- project.py
+++ project.py
@@ -1,1 +1,20 @@
-print("Hello Git!")
+import pygame
+
+pygame.init()
+gameDisplay = pygame.display.set_mod((800,600))
+pygame.display.set_caption('A bit Racey')
+clock = pygame.time.Clock()
+
+crashed = False
+while not crashed:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            crashed = True
+
+        print(event)
+
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
+quit()
