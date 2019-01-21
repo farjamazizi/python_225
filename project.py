0c648852a425af8fee3e8646ced366b0e4cd07c8
farjamazizi <46857447+farjamazizi@users.noreply.github.com>
2019-01-21 23:35:44 +0330

    step1.1(intro) change mode project.py

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
 import pygame
 
 pygame.init()
-gameDisplay = pygame.display.set_mod((800,600))
+gameDisplay = pygame.display.set_mode((800,600))
 pygame.display.set_caption('A bit Racey')
 clock = pygame.time.Clock()
 
